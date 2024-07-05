7aabd7db4821e52ac70eb311b10e0fcc56fcc51a
Christian Adler <chris.adler@gmx.de>
2024-07-05 20:18:19 +0200

    add more fnc

--- main.mjs
+++ main.mjs
@@ -33,66 +33,42 @@ let yMin = -1;
 let yMax = 1;
 
 let numValues = 5000;
+let step = 5 / numValues;
+
+let fillFnc = () => {
+};
 
 const sequence = [];
 
-const fillSequenceExp = () => {
-  let step = 5 / numValues;
-  yMax = Number.MIN_SAFE_INTEGER;
-  yMin = Number.MAX_SAFE_INTEGER;
-  sequence.splice(0, sequence.length);
-
-  for (let x = 0; x <= 5; x += step) {
-    const y = -Math.exp(-x) * Math.cos(2 * Math.PI * x);
-    yMin = Math.min(yMin, y);
-    yMax = Math.max(yMax, y);
-    sequence.push(new Vector(x, y));
-  }
-
-  xMin = sequence[0].x;
-  xMax = sequence[sequence.length - 1].x;
-};
-const fillSequenceSinCos = () => {
-  let step = 5 / numValues;
-  yMax = Number.MIN_SAFE_INTEGER;
-  yMin = Number.MAX_SAFE_INTEGER;
-  sequence.splice(0, sequence.length);
-
-  for (let x = 0; x <= 5; x += step) {
-    const y = -(Math.cos(2 * Math.PI * x * 2) + Math.sin(Math.PI * x));
-    yMin = Math.min(yMin, y);
-    yMax = Math.max(yMax, y);
-    sequence.push(new Vector(x, y));
-  }
-
-  xMin = sequence[0].x;
-  xMax = sequence[sequence.length - 1].x;
-};
-const fillSequenceRand = () => {
-  let step = 5 / numValues;
-  yMax = Number.MIN_SAFE_INTEGER;
-  yMin = Number.MAX_SAFE_INTEGER;
-  sequence.splice(0, sequence.length);
-
-  for (let x = 0; x <= 5; x += step) {
-    const y = -Math.random();
-    yMin = Math.min(yMin, y);
-    yMax = Math.max(yMax, y);
-    sequence.push(new Vector(x, y));
-  }
-
-  xMin = sequence[0].x;
-  xMax = sequence[sequence.length - 1].x;
-};
-
 const dataFnc = {
-  Exp: fillSequenceExp,
-  Cos: fillSequenceSinCos,
-  Random: fillSequenceRand,
+  Exp: (x) => -Math.exp(-x) * Math.cos(2 * Math.PI * x),
+  Cos: (x) => -(Math.cos(2 * Math.PI * x * 2) + Math.sin(Math.PI * x)),
+  Sin: (x) => -Math.sin(954 * x) - 2 * Math.cos(x),
+  Cut: (x) => Math.exp(-x) * Math.min(Math.sin(2 * Math.PI * x), 0.3),
+  ATan: (x) => -(Math.sin(Math.PI * x / 5) - Math.atan(x * 2)),
+  Steps: (x) => ((x - 0.2) * Math.sin(1 / (x - 0.2)) + x + 0.8) * (10 * (x - 0.1) ^ 2 + 0.9),
+  Random: () => -Math.random(),
 };
 
-let fillFnc = dataFnc['Exp'];
-fillFnc();
+const fill = () => {
+  step = 5 / numValues;
+  yMax = Number.MIN_SAFE_INTEGER;
+  yMin = Number.MAX_SAFE_INTEGER;
+  sequence.splice(0, sequence.length);
+
+  for (let x = 0; x <= 5; x += step) {
+    const y = fillFnc(x);
+    yMin = Math.min(yMin, y);
+    yMax = Math.max(yMax, y);
+    sequence.push(new Vector(x, y));
+  }
+
+  xMin = sequence[0].x;
+  xMax = sequence[sequence.length - 1].x;
+};
+
+fillFnc = dataFnc['Exp'];
+fill();
 
 const drawSeq = (ctx, seq) => {
   ctx.beginPath();
@@ -161,11 +137,18 @@ numValuesInput.addEventListener("input", (evt) => {
   const target = evt.target;
   numValues = Math.floor(Math.pow(parseInt(target.value), 2.2));
   target.nextSibling.innerText = numValues;
-  fillFnc();
+  fill();
 });
 const selectDataFncInput = document.getElementById('selectDataFnc');
+for (const key of Object.keys(dataFnc)) {
+  const optionElement = document.createElement('option');
+  // <option value="Exp">Exp</option>
+  optionElement.setAttribute('value', key);
+  optionElement.innerText = key;
+  selectDataFncInput.appendChild(optionElement);
+}
 selectDataFncInput.addEventListener("change", (evt) => {
   const target = evt.target;
   fillFnc = dataFnc[target.value];
-  fillFnc();
+  fill();
 });
